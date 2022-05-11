613709eef594a61b4b350b23fef42919e2aa209b
Isaque Schuwarte <isaque_schuwarte@hotmail.com>
2022-05-10 22:38:32 -0300

    eslint correction

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
 
-export default props => {
-  let classes = 'button ';
-  classes += props.operation ? 'operation' : '';
-  classes += props.double ? 'double' : '';
-  classes += props.triple ? 'triple' : '';
+export default function Button(props) {
+  let classes = "button ";
+  classes += props.operation ? "operation" : "";
+  classes += props.double ? "double" : "";
+  classes += props.triple ? "triple" : "";
 
   return (
-    <button onClick={e => props.click && props.click(props.label)} className={classes}>
+    <button
+      onClick={(e) => props.click && props.click(props.label)}
+      className={classes}
+    >
       {props.label}
     </button>
-  )
+  );
 }
